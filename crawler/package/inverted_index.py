0c237e93e0e812babe9bdbdd934cb44b27e8f5fd
topdelir1 <nathan.seva@outlook.fr>
2015-03-31 21:58:33 +0200

    Optimize indexing

--- crawler/package/inverted_index.py
+++ crawler/package/inverted_index.py
@@ -16,12 +16,15 @@ class InvertedIndex:
 		"""Build the InvertedIndex manager.
 
 		example :
-		'word'{1:12,20:39}'site'{2:14}
+		'word'1:12,20:39'site'2:14
 		the word 'word' is 20 times in the first document,
 		the word 'site' is 14 times in the document 2.
 
+		self.inverted_index = {'a': "'avion1:2'", 'b': "'bateau'2:2"}
+
 		"""
-		self.inverted_index = str()
+		self.inverted_index = dict()
+		self.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 		self.STOPWORDS = dict()
 
 	def setStopwords(self, STOPWORDS):
@@ -36,35 +39,66 @@ class InvertedIndex:
 		"""Return inverted inverted_index."""
 		return self.inverted_index
 
-	def append_doc(self, webpage_infos, doc_id):
-		"""Add all words of a doc in the inverted_index."""
-		words_to_add = webpage_infos['keywords']
-		title_keywords = self.generate_keywords(webpage_infos['title'], webpage_infos['language'])
-		words_to_add.extend(title_keywords)
+	def append_doc(self, keywords, doc_id):
+		"""Add all words of a doc in the inverted_index.
 
+		Take a list of keywords  and return a list contain modify dicts.
+		The list given must be contain page keywords and title keywords, and purhap description keywords.
+
+		"""
 		beginning = time()
-
-		for word in words_to_add:
+		new_inverted_index = dict()
+		for word in keywords:
 			if time() - beginning < INDEXING_TIMEOUT:
-				occurence = words_to_add.count(word)
-
-				word_position = self.inverted_index.find("'" + word + "'")
-				if word_position != -1:
-					# if the word is in the inverted index :
-					end_position = self.inverted_index.find('}', word_position)
-					if self.inverted_index.find(doc_id, word_position, end_position) == -1:
-						quote_position = self.inverted_index.find('\'', end_position)
-						self.inverted_index = self.inverted_index[:end_position] + ',' + doc_id + ':' + str(occurence) + self.inverted_index[quote_position-1:]
+				occurence = str(keywords.count(word))
+				first_letter = word[0]
+				if first_letter in self.alphabet:
+					# it's a letter
+					if first_letter in self.inverted_index.keys():
+						# the key exists in th dict
+						# take the value of the dict
+						inverted_index = self.inverted_index[first_letter]
+					else:
+						# add the key and value in the dict
+						self.inverted_index[first_letter] = inverted_index = str()
 				else:
-					# if we need to add it in the inverted_index :
-					self.inverted_index += "'" + word + "'{" + doc_id + ':' + str(occurence) +  '}'
+					# the first char isn't a letter
+					if '_' in self.inverted_index.keys():
+						inverted_index = self.inverted_index['_']
+					else:
+						self.inverted_index['_'] = inverted_index = str()
+				word_position = inverted_index.find("'" + word + "'")
+				if word_position != -1:
+					# the word is in the inverted-index
+					word_position += 1
+					end_position = inverted_index.find("}", word_position)
+					# add doc and occurence in inverted-index :
+					doc_id_position = inverted_index.find(doc_id + ':', word_position, end_position)
+					quote_position = inverted_index.find("'", end_position)
+					if doc_id_position != -1:
+						# there is already the doc to the word, update occurence :
+						inverted_index = inverted_index[:doc_id_position+2] + occurence + inverted_index[doc_id_position + len(occurence) +2:]
+					else:
+						# there isn't the doc to this word :
+						if quote_position != -1:
+							inverted_index = inverted_index[:end_position] + ',' + doc_id + ':' + occurence + inverted_index[quote_position-1:]
+						else:
+							# if there is only one word :
+							inverted_index = inverted_index[:end_position] + ',' + doc_id + ':' + occurence + '}'
+				else:
+					# adding the word in the inverted-index :
+					inverted_index += "'" + word + "'{" + doc_id + ':' + occurence + '}'
+
+				new_inverted_index[first_letter] = inverted_index
+				self.inverted_index[first_letter] = inverted_index
+
 			else:
 				speak('Indexing too long : pass', 23)
-				return True
+				return None
 
-		self.save_keyword(list(set(words_to_add))) # tests
+		#self.save_keyword(str(list(set(words_to_add)))) # tests
 
-		return False
+		return new_inverted_index
 
 	def generate_keywords(self, title, language):
 		if language == 'fr':
@@ -102,5 +136,5 @@ class InvertedIndex:
 	def save_keyword(self, words_to_add):
 		"""Save the keywords in a file to check errors."""
 		with open(DIR_OUTPUT + 'save_keywords.txt', 'a', encoding='utf-8', errors='replace') as myfile:
-			myfile.write(str(words_to_add))
+			myfile.write(words_to_add)
 			myfile.write('\n\n')
--- crawler/tests/test_crawler.py
+++ crawler/tests/test_crawler.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 from package.module import *
-from package.searches import *
+from package.searches import SiteInformations
+from package.inverted_index import InvertedIndex
 
 class TestCrawlerBase(object):
     """Base class for all crawler test classes."""
@@ -10,11 +11,14 @@ class TestCrawlerBase(object):
         self.url = 'http://www.example.fr'
         self.language = 'fr'
         self.STOPWORDS = {'fr':('mot', 'pour')}
+        self.inverted_index = {'t': "'train'{1:2}"}
+        self.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 class TestCrawlerBasic(TestCrawlerBase):
     def test_clean_text(self):
         text = ('Sample text with non-desired \r whitespaces \t chars \n')
         text = clean_text(text)
+
         assert not '\n' in text and not '\r' in text and not '\t' in text
 
     def test_clean_links(self):
@@ -29,3 +33,13 @@ class TestCrawlerBasic(TestCrawlerBase):
 
     def test_remove_duplicates(self):
         assert remove_duplicates(['mot', 'mot']) == ['mot']
+
+    def test_append_doc(self):
+        inverted_index = InvertedIndex.append_doc(self, ['voiture', 'mot', 'tobogan'], '2')
+        assert inverted_index == {'m': "'mot'{2:1}", 'v': "'voiture'{2:1}", 't': "'train'{1:2}'tobogan'{2:1}"}
+
+        inverted_index = InvertedIndex.append_doc(self, ['voiture', 'mot', 'mot', 'mot', 'chanteur', 'avion'], '3')
+        assert inverted_index == {'a': "'avion'{3:1}", 'c': "'chanteur'{3:1}", 'm': "'mot'{2:1,3:3}", 'v': "'voiture'{2:1,3:1}"}
+
+        inverted_index = InvertedIndex.append_doc(self, ['avion', 'avion'], '3')
+        assert inverted_index == {'a': "'avion'{3:2}"}
