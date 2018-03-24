a948a41d0a17a14b757ad153832a5e212188b2e9
Thykof <nathan.seva@outlook.fr>
2018-03-24 19:56:24 +0100

    Add gobal_test in run_tests

--- crawler/tests/run_tests.py
+++ crawler/tests/run_tests.py
@@ -15,9 +15,12 @@ import os
 
 import pytest
 
-from crawler.tests.test_data import reset
+try:
+    from crawler.tests.test_data import reset
+except ModuleNotFoundError:
+    from test_data import reset
 
-def run_tests():
+def run_tests(local=False):
     os.chdir('crawler/tests')
     args = ['--strict', '--verbose']
     args.append('swiftea_bot_test.py')
@@ -25,19 +28,11 @@ def run_tests():
     args.append('database_test.py')
     args.append('index_test.py')
     args.append('crawler_test.py')
+    if local:
+        args.append('global_test.py')
     errno = pytest.main(args)
     reset()
     return errno
 
-def tests(args=['--strict', '--verbose']):
-    if 'file' not in args:
-        if __name__ == '__main__':
-            args.append(path + 'global_test.py ')
-        args.append(path + 'swiftea_bot_test.py ')
-        args.append(path + 'crawling_test.py ')
-        args.append(path + 'database_test.py ')
-        args.append(path + 'index_test.py ')
-        args.append(path + 'crawler_test.py ')
-    errno = pytest.main(args)
-    reset()
-    return errno
+if __name__ == '__main__':
+    run_tests(True)
