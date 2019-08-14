52bd4c00610f1e5dfa41c41d3719bda11c1dd9ca
Nathan Seva <thykof@protonmail.ch>
2019-08-14 21:39:35 +0200

    run.py: change url

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from crawler.main import main
 
 
 if __name__ == '__main__':
-    c = main(url="https://swiftea.yo.fr/", target_level=2)
+    c = main(url="https://github.io/", target_level=1)
     c.start()
